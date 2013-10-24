336a4b1f34d6e03283177a2da8d8a124b690d4e3
Michael Radionov <micrddrgn@gmail.com>
2013-10-24 23:19:51 +0300

    Copied Symfony methods 'camelize' and 'underscore' to use independently in utils classes

--- Utils/StringUtils.php
+++ Utils/StringUtils.php
@@ -77,4 +77,42 @@ class StringUtils
 
     }
 
+    /**
+     * Repeat Symfony original function not to have dependencies on it,
+     * to use this function in another util classes.
+     * 
+     * Camelizes a string.
+     *
+     * @param string $id A string to camelize
+     * @return string The camelized string
+     */
+    public static function camelize($id)
+    {
+        return preg_replace_callback(
+            '/(^|_|\.)+(.)/', 
+            function ($match) { 
+                return ('.' === $match[1] ? '_' : '').strtoupper($match[2]); 
+            }, 
+            $id
+        );
+    }
+
+    /**
+     * Repeat Symfony original function not to have dependencies on it,
+     * to use this function in another util classes
+     * 
+     * A string to underscore.
+     *
+     * @param string $id The string to underscore
+     * @return string The underscored string
+     */
+    public static function underscore($id)
+    {
+        return strtolower(preg_replace(
+            array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), 
+            array('\\1_\\2', '\\1_\\2'), 
+            strtr($id, '_', '.')
+        ));
+    }
+
 }
